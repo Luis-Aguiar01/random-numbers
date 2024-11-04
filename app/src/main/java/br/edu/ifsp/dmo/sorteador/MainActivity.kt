e05f50b81b3589f1ff2b5bfabab69f45517109ae
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-04 16:17:53 -0300

    Configurando o ViewBinding no projeto

--- app/src/main/java/br/edu/ifsp/dmo/sorteador/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/sorteador/MainActivity.kt
@@ -2,10 +2,15 @@ package br.edu.ifsp.dmo.sorteador
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.sorteador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
