bf7c5602ea801f14392ed4663a1879e2e7e7342a
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-09 07:20:41 -0300

    Criação da função que atualiza as informações da tela com base no tipo de sorteio escolhido pelo usuário

--- app/src/main/java/br/edu/ifsp/dmo/sorteador/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/sorteador/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.sorteador.R
 import br.edu.ifsp.dmo.sorteador.databinding.ActivityMainBinding
 import br.edu.ifsp.dmo.sorteador.model.Draw
 
@@ -33,6 +34,8 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                     Draw(limit)
                 else
                     Draw()
+                
+                updateUI()
             }
             binding.generateButton -> {
                 binding.resultTextView.text = draw.getNumber().toString()
@@ -43,5 +46,12 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     private fun setClickListener() {
         binding.generateButton.setOnClickListener(this)
         binding.generateButton.setOnClickListener(this)
+    }
+
+    private fun updateUI() {
+        val str = String.format("Intervalo de 1 à %,d.", draw.getHighBorder())
+        binding.limitTextView.text = str
+        binding.inputEditText.text.clear()
+        binding.resultTextView.text = getString(R.string.result_edit_text)
     }
 }
